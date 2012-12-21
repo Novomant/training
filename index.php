56f50260ec1787d811cba9340c82d6ff6d8f7cd0
KPakshaev <=>
2012-12-21 12:42:33 +0400

    indentation

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-        <head>
-            <title>Погода</title>
-        </head>
+    <head>
+        <title>Погода</title>
+    </head>
     <body>
         <form method="POST" action="/">
             Город: <input type="text" name="town">
@@ -14,26 +14,26 @@
 <?php
 //Проверка нажатия кнопки
 if (isset($_REQUEST['ok']))
-    {
+{
     $town = $_POST['town'];
     $date = $_POST['date'];
     // Вставляем переменные, введеные пользователем, в адрес запроса. Возвращаем строку, в которой находится xml
     $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
     //Превращаем полученную строку xml в объект
     $xml = simplexml_load_string($xmlStr);
-        // Если пользователь вводит неверную дату или города нет в базе
-        if (isset($xml->weather[1]) or (isset($xml->error->msg)) )
-            {
-            echo "Ошибка! Неправильно введена дата или города нет в базе данных";
-            }
-                //Если же пользователь ввел все верно, выдаем данные о запрашиваемой дате и погоду
-                else
-                    {
-                    echo "Вы выбрали дату: ".$xml->weather[0]->date;
-                    echo "<br>Максимальная температура в этот день: ".$xml->weather[0]->tempMaxC;
-                    echo "<br>Минимальная температура в этот день: ".$xml->weather[0]->tempMinC;
-                    }
+    // Если пользователь вводит неверную дату или города нет в базе
+    if (isset($xml->weather[1]) or (isset($xml->error->msg)) )
+    {
+        echo "Ошибка! Неправильно введена дата или города нет в базе данных";
     }
+    //Если же пользователь ввел все верно, выдаем данные о запрашиваемой дате и погоду
+    else
+    {
+        echo "Вы выбрали дату: ".$xml->weather[0]->date;
+        echo "<br>Максимальная температура в этот день: ".$xml->weather[0]->tempMaxC;
+        echo "<br>Минимальная температура в этот день: ".$xml->weather[0]->tempMinC;
+    }
+}
 ?>
     </body>
 </html>
