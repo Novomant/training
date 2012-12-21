a614fdbb41c8e2e090f6798032bb73e0c73a8390
Novomant <novomant@gmail.com>
2012-12-21 14:21:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Погода</title>
-    </head>
+        <head>
+            <title>Погода</title>
+        </head>
     <body>
         <form method="POST" action="/">
             Город: <input type="text" name="town">
             <br>
             Дата в формате ГГГГ-ММ-ДД: <input type="text" name="date">
             <br><br>
-           <input type="submit" name="ok" value="Отправить">
+            <input type="submit" name="ok" value="Отправить">
         </form>
-    </body>
 <?php
-@$town = $_POST['town'];
-@$date = $_POST['date']; //Переменные для данных, введенные пользователем. Оператор @ использован по причине остутствия ошибки, так как переменные еще не определены в начале выполнения сценария
-if (isset($town) and isset($date)) //Если пользователь ввел данные в оба поля
-{
-    $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');// Вставляем переменные, введеные пользователем, в адрес запроса. Возвращаем строку, в которой находится xml
-    $xml = simplexml_load_string($xmlStr); //Превращаем полученную строку xml в объект
-    if (!isset($xml->weather[0]->tempMaxC)) // В случае отсутсвия данных выдаем ошибку
+//Проверка нажатия кнопки
+if (isset($_REQUEST['ok']))
     {
-        exit ("Ошибка! Нет такого города в базе данных или неправильно введены данные");
+    $town = $_POST['town'];
+    $date = $_POST['date'];
+    // Вставляем переменные, введеные пользователем, в адрес запроса. Возвращаем строку, в которой находится xml
+    $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
+    //Превращаем полученную строку xml в объект
+    $xml = simplexml_load_string($xmlStr);
+        // Если пользователь вводит неверную дату или города нет в базе
+        if (isset($xml->weather[1]) or (isset($xml->error->msg)) )
+            {
+            echo "Ошибка! Неправильно введена дата или города нет в базе данных";
+            }
+                //Если же пользователь ввел все верно, выдаем данные о запрашиваемой дате и погоду
+                else
+                    {
+                    echo "Вы выбрали дату: ".$xml->weather[0]->date;
+                    echo "<br>Максимальная температура в этот день: ".$xml->weather[0]->tempMaxC;
+                    echo "<br>Минимальная температура в этот день: ".$xml->weather[0]->tempMinC;
+                    }
     }
-    echo "Вы выбрали дату: ".$xml->weather[0]->date;
-    echo "<br>Максимальная температура в этот день: ".$xml->weather[0]->tempMaxC;
-    echo "<br>Минимальная температура в этот день: ".$xml->weather[0]->tempMinC;
-}
 ?>
+    </body>
 </html>
