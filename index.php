6c4d0b786c1b223804505a44a884ae6bd39c1f36
Novomant <novomant@gmail.com>
2013-01-02 15:51:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,42 +12,58 @@
             <input type="submit" name="ok" value="Отправить">
         </form>
 <?php
-//Подключаем расширение Memcache
-$memcache = new Memcache;
-$memcache->connect('localhost', 11211);
-    if (isset($_REQUEST['ok']))
+if (isset($_REQUEST['ok']))
+{
+    //Подключаем расширение Memcache
+    $memcache = new Memcache;
+    $memcache -> connect('localhost', 11211);
+    $town = $_POST['town'];
+    $date = $_POST['date'];
+    //Устанавливаем уникальное значение ключа для хэша (город+дата)
+    $key = $date."and".$town;
+    //Получаем данные из хэша
+    $get = $memcache -> get($key);
+    //В случае наличия ячейки в полученном из хэша массиве выводим данные о погоде
+    if ($get["t_max"] != null)
     {
-        $town = $_POST['town'];
-        $date = $_POST['date'];
-        //Устанавливаем уникальное значение ключа для хэша (город+дата)
-        $key = $date."and".$town;
-        //Получаем данные из хэша
-        $get = $memcache->get($key);
-        echo $get;
-        //Если хэш пуст, делаем запрос на сервер погоды
-        if ($get == null)
+        echo "<br>Вы выбрали город: ".$get["town"];
+        echo "<br>Вы выбрали дату: ".$get["date"];
+        echo "<br>Максимальная температура в этот день: ".$get["t_max"];
+        echo "<br>Минимальная температура в этот день: ".$get["t_min"];
+    }
+    //Если хэш пуст, делаем запрос на сервер погоды
+    if ($get == null)
+    {
+        $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
+        //Превращаем полученную строку xml в объект
+        $xml = simplexml_load_string($xmlStr);
+        //Создаем массив, значения которого запишутся в ключ переменной для хэша
+        $info = array(
+        "town" => $xml->request->query->asXML(),
+        "date" => $xml->weather[0]->date->asXML(),
+        "error_1" => $xml->weather[1],
+        "error_2" => $xml->error->msg,
+        "t_max" => $xml->weather->tempMaxC->asXML(),
+        "t_min" => $xml->weather->tempMinC->asXML());
+        unset($xml);
+        //В случае неверного запроса придет одно из этих двух значений. Выводим ошибку
+        if (isset($info["error_1"]) or (isset($info["error_2"])))
         {
-            $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
-            //Превращаем полученную строку xml в объект
-            $xml = simplexml_load_string($xmlStr);
-            //В случае неверного запроса придет одно из этих двух значений. Выводим ошибку
-            if (isset($xml->weather[1]) or (isset($xml->error->msg)) )
-            {
-                echo "Ошибка! Неправильно введена дата или города нет в базе данных";
-            }
-            //Если запрос корректен, то выводим информацию по городу и дате
-            else
-            {
-                 //Записываем данные по городу и дате в переменную, которая является значением ключа хэша
-                $info ="<br>Вы выбрали город: ".$xml->request->query->asXML()."<br>Вы выбрали дату: ".$xml->weather[0]->date->asXML()."<br>Максимальная температура в этот день: ".$xml->weather[0]->tempMaxC->asXML()."<br>Минимальная температура в этот день: ".$xml->weather[0]->tempMinC->asXML();
-                $set = $memcache->set($key, $info);
-                echo "<br>Вы выбрали город: ".$xml->request->query;
-                echo "<br>Вы выбрали дату: ".$xml->weather->date;
-                echo "<br>Максимальная температура в этот день: ".$xml->weather->tempMaxC;
-                echo "<br>Минимальная температура в этот день: ".$xml->weather->tempMinC;
-            }
+            echo "Ошибка! Неправильно введена дата или города нет в базе данных";
+        }
+        //Если запрос к серверу погоды корректен, то
+        else
+        {
+             //записываем данные по городу и дате в переменную, которая является значением ключа хэша
+            $set = $memcache->set($key, $info, 604000);
+            //и выводим данные по городу и дате
+            echo "<br>Вы выбрали город: ".$info["town"];
+            echo "<br>Вы выбрали дату: ".$info["date"];
+            echo "<br>Максимальная температура в этот день: ".$info["t_max"];
+            echo "<br>Минимальная температура в этот день: ".$info["t_min"];
         }
     }
+}
 ?>
     </body>
 </html>
