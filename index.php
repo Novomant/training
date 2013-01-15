252938ad2396f7072eabe7fcd3121e4209d10f7b
Novomant <novomant@gmail.com>
2013-01-15 10:35:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,25 +14,71 @@
 <?php
 if (isset($_REQUEST['ok']))
 {
+    //Проверяем, установлен ли Memcache
+    if (!class_exists('Memcache'))
+    {
+        echo "Расширение Memcache не установлено.";
+    }
+    else
+    {
+        echo "Расширение Memcache установлено";
+    }
     //Подключаем расширение Memcache
     $memcache = new Memcache;
-    $memcache -> connect('localhost', 11211);
+    //Подключаем список серверов
+    include 'servers.php';
+    //Устанавливаем уникальное значение ключа для кэша (город+дата)
     $town = $_POST['town'];
     $date = $_POST['date'];
-    //Устанавливаем уникальное значение ключа для кэша (город+дата)
-    $key = $date."and".$town;
+    $key = $date.$town;
+        //Подключаемся к первому серверу
+    $memcache -> connect($server_1, 11211);
     //Получаем данные из кэша
     $info = $memcache -> get($key);
+	//Если данных нет на первом сервере, то обращаемся к остальным, по очереди
+	if (!$info)
+	{
+		$memcache -> connect($server_2, 11211);
+		$info = $memcache -> get($key);
+		if (!$info)
+		{
+			$memcache -> connect($server_3, 11211);
+			$info = $memcache -> get($key);
+			if (!$info)
+			{
+				$memcache -> connect($server_4, 11211);
+				$info = $memcache -> get($key);
+			}
+		}
+	}
 
-    //Если кэш пуст, делаем запрос на сервер погоды
-    if ($info == null)
+    //Если кэш всех серверов пуст, делаем запрос на сервер погоды
+    if (!$info)
     {
+        //Подключаемся к случайному серверу для записи данных
+        $rand = rand(1, 4);
+        if ($rand == 1)
+        {
+            $memcache -> connect($server_1, 11211);
+        }
+        if ($rand == 2)
+        {
+            $memcache -> connect($server_2, 11211);
+        }
+        if ($rand == 3)
+        {
+            $memcache -> connect($server_3, 11211);
+        }
+        if ($rand == 4)
+        {
+            $memcache -> connect($server_4, 11211);
+        }
         $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
         //Превращаем полученную строку xml в объект
         $xml = simplexml_load_string($xmlStr);
-        //Создаем массив, значения которого запишутся в ключ переменной для хэша
+        //Создаем массив, значения которого запишутся в ключ переменной для кэша
         $info = array(
-           "town" => $xml->request->query->asXML(),
+            "town" => $xml->request->query->asXML(),
             "date" => $xml->weather[0]->date->asXML(),
             "error_1" => $xml->weather[1],
             "error_2" => $xml->error->msg,
@@ -71,5 +117,5 @@ if (isset($_REQUEST['ok']))
     }
 }
 ?>
-    </body>
+</body>
 </html>
