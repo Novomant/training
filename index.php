6bdf7dd1b4ed2894a053cbffda927636e2c7e722
Novomant <novomant@gmail.com>
2013-01-18 12:55:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,16 +43,16 @@ if (isset($_REQUEST['ok']))
 			}
 		}
 	}
-    //Если кэш всех серверов пуст или Memcache не работает, то делаем запрос на сервер погоды
-    if (!$info or $isWorksServer==false)
-    {
-	    $xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
+	//Если кэш всех серверов пуст или Memcache не работает, то делаем запрос на сервер погоды
+	if (!$info or $isWorksServer==false)
+	{
+		$xmlStr = file_get_contents('http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$town.'&date='.$date.'&key=50635796a4181608121312&format=xml');
 	//Превращаем полученную строку xml в объект
-        $xml = simplexml_load_string($xmlStr);
+		$xml = simplexml_load_string($xmlStr);
 		//Массив с ошибками
 		$info = array (
 			"error_1" => $xml->weather[1],
-            "error_2" => $xml->error->msg
+			"error_2" => $xml->error->msg
 		);
 		$isErrorData = (isset($info["error_1"]) or isset($info["error_2"]));
 		// Если ошибка есть
@@ -61,47 +61,47 @@ if (isset($_REQUEST['ok']))
 			echo "Ошибка! Неправильно введена дата или города нет в базе данных";
 		}
 		//Если ошибки нет
-        if (!$isErrorData)
+		if (!$isErrorData)
 			//Создаем массив, значения которого запишутся в ключ переменной для кэша
 			$info = array(
-            "town" => $xml->request->query->asXML(),
-            "date" => $xml->weather[0]->date->asXML(),
-            "error_1" => $xml->weather[1],
-            "error_2" => $xml->error->msg,
-            "t_max" => $xml->weather->tempMaxC->asXML(),
-            "t_min" => $xml->weather->tempMinC->asXML()
-        );
-        unset($xml);
+			"town" => $xml->request->query->asXML(),
+			"date" => $xml->weather[0]->date->asXML(),
+			"error_1" => $xml->weather[1],
+			"error_2" => $xml->error->msg,
+			"t_max" => $xml->weather->tempMaxC->asXML(),
+			"t_min" => $xml->weather->tempMinC->asXML()
+		);
+		unset($xml);
 
-        // Если в данных полученных с сервера нет ошибки и Memcache работает - сохраним их на случайном и последнем подключенном сервере.
-        if (!$isErrorData and $isWorksServer==true)
-        {
-            //Рандом для сервера, к которому будем подключаться
-            $maxServ = count($servers)-1;
+		// Если в данных полученных с сервера нет ошибки и Memcache работает - сохраним их на случайном и последнем подключенном сервере.
+		if (!$isErrorData and $isWorksServer==true)
+		{
+			//Рандом для сервера, к которому будем подключаться
+			$maxServ = count($servers)-1;
 			$randServ = rand (0, $maxServ);
-            //Если рандом не выпал на последний сервер (к которому мы уже подключены), то отключаемся от него
+			//Если рандом не выпал на последний сервер (к которому мы уже подключены), то отключаемся от него
 			if ($maxServ != $randServ)
 			{
 				$memcache->close($servers["$maxServ"]);
 			}
-            //Если рандом на любом сервере, кроме последнего, то подключаемся к нему
-            else
-            {
-                $memcache -> connect($servers["$randServ"], 11211);
-                //записываем данные по городу и дате в переменную, которая является значением ключа кэша
-                $set = $memcache->set($key, $info, 604000);
-            }
-        }
+			//Если рандом на любом сервере, кроме последнего, то подключаемся к нему
+			else
+			{
+				$memcache -> connect($servers["$randServ"], 11211);
+				//записываем данные по городу и дате в переменную, которая является значением ключа кэша
+				$set = $memcache->set($key, $info, 604000);
+			}
+		}
 	}
 
-    // Если в данных полученные из кеша либо с сервера нет ошибки - выведем их
-    if (!$isErrorData)
-    {
-        echo "<br>Вы выбрали город: ".$info["town"];
-        echo "<br>Вы выбрали дату: ".$info["date"];
-        echo "<br>Максимальная температура в этот день: ".$info["t_max"];
-        echo "<br>Минимальная температура в этот день: ".$info["t_min"];
-    }
+	// Если в данных полученные из кеша либо с сервера нет ошибки - выведем их
+	if (!$isErrorData)
+	{
+		echo "<br>Вы выбрали город: ".$info["town"];
+		echo "<br>Вы выбрали дату: ".$info["date"];
+		echo "<br>Максимальная температура в этот день: ".$info["t_max"];
+		echo "<br>Минимальная температура в этот день: ".$info["t_min"];
+	}
 }
 ?>
 	</body>
