245f5017f4ec621bbf12319f63c53af3ac2d7d0b
Novomant <novomant@gmail.com>
2013-04-18 21:12:42 +0700

    up

--- module/Class.php
+++ module/Class.php
@@ -39,6 +39,9 @@ class BaseWeather extends Base
 {
 	public $town;
 	public $date;
+	public $isErrorIncorrectDate = false;
+	public $isErrorDate = false;
+	public $isErrorXML = false;
 
 	public function setTown($town)
 	{
@@ -47,8 +50,17 @@ class BaseWeather extends Base
 
 	public function setDate($date)
 	{
-		$this->date = $date;
+		//Создаем объект с исключением (если объект не создался) - фиксируем ошибку
+		try
+		{
+			$this->date = new DateTime($date);
+		}
+		catch (Exception $e)
+		{
+			$this->isErrorIncorrectDate = true;
+		}
 	}
+
 	public function getTown()
 	{
 		return $this->town;
@@ -62,7 +74,7 @@ class BaseWeather extends Base
 	//Ключ для мемкэша
 	public function getKeyMemcache()
 	{
-		return $this->getTown().$this->getDate();
+		return $this->getTown().$this->getDate()->format('Y-m-d');
 	}
 
 	//Проверяем, работает ли Мемкэш
@@ -82,34 +94,135 @@ class BaseWeather extends Base
 		}
 	}
 
-			//Подключаемся к каждому серверу по очереди. Если данные ключа заполнились или достигнут последний элемент массива, то прерываем цикл
+	//Подключаемся к каждому серверу по очереди. Если данные ключа заполнились или достигнут последний элемент массива, то прерываем цикл
 	public function connect()
 	{
-		$memcache = new Memcache;
-		for ($this->curr = reset($this->servers); !empty($this->curr) && empty($this->info); $this->curr = next($this->servers))
+		$this->memcache = new Memcache;
+		for ($curr = reset($this->servers); !empty($curr) && empty($this->info); $curr = next($this->servers))
 		{
-			$memcache -> connect($this->curr, 11211);
-			$this->info = $memcache -> get($this->getTown().$this->getDate());
+			$this->memcache -> connect($curr, 11211);
+			$this->info = $this->memcache -> get($this->getTown().$this->getDate()->format('Y-m-d'));
 		}
 	}
 
-	//Разбираем дату на части
-	public function separationDate()
+	public function loadApi()
 	{
-		if (!$this->isWorkServer() or !$this->info)
+		//Если кэш всех серверов пуст или Memcache не работает, то делаем запрос на сервер погоды
+		if (!$this->info or $this->isWorkServer() == false)
 		{
-			$this->del = explode("-", $this->date);
-			// Распределяем по переменным
-			list ($this->year, $this->month, $this->day) = $this->del;
+			$Y = $this->getDate()->format('Y');
+			$m = $this->getDate()->format('m');
+			$d = $this->getDate()->format('d');
+			$Ymd = $this->getDate()->format('Y-m-d');
+			//Список Api
+			$apiUrl = array(
+				'http://dev.4otaku.ru/weather.php?city='.$this->getTown().'&year='.$Y.'&month='.$m.'&day='.$d.'',
+				'http://free.worldweatheronline.com/feed/weather.ashx?cc=no&num_of_days=2&q='.$this->getTown().'&date='.$Ymd.'&key=50635796a4181608121312&format=xml'
+			);
+			//Получаем данные с сервера погоды, выбранного случайно и только в случае, если запрошенный город не иностранный
+			for (true; !isset($this->loadApi) or $this->loadApi == "Fuck off, capitalist pig!"; $this->loadApi = file_get_contents ($apiUrl[$this->randApi]))
+			{
+				$this->randApi = rand(0,1);
+			}
 		}
 	}
 
+	public function decodeJson()
+	{
+		if (!$this->info or $this->isWorkServer() == false)
+		{
+			//Если на рандом выпал Api 1, то работаем с форматом json
+			if ($this->randApi==0)
+			{
+				//Декодируем строку json
+				$json = json_decode($this->loadApi);
+				//Заполняем массив данными
+				$this->info = array(
+					"town" => $this->getTown(),
+					"t_max" => $json->{'Max'},
+					"t_min" => $json->{'Min'});
+				//Переводим температуру в шкалу Цельсия
+				$this->info["t_max"]=($this->info["t_max"] - 32)*(5/9);
+				$this->info["t_min"]=($this->info["t_min"] - 32)*(5/9);
+				$this->info["t_max"]=intval($this->info["t_max"]);
+				$this->info["t_min"]=intval($this->info["t_min"]);
+				unset($json);
+			}
+		}
+	}
 
+	public function decodeXml()
+	{
+		if (!$this->info or $this->isWorkServer() == false)
+		{
+			//Если на рандом выпал Api 2, то работаем с форматом xml
+			if ($this->randApi==1)
+			{
+				//Превращаем полученную строку xml в объект
+				$xml = simplexml_load_string($this->loadApi);
+				#var_dump($xml);
+				//Массив с ошибками
+				$this->info = array (
+					"error_1" => $xml->weather[1],
+					"error_2" => $xml->error->msg);
+				if (isset($this->info["error_1"]) or isset($this->info["error_2"]))
+				{
+					$this->isErrorXML = true;
+				}
+				//Если нет ошибки, то заполняем массив
+				if (!$this->isErrorXML)
+				{
+					$this->info = array(
+						"town" => $xml->request->query->asXML(),
+						"t_max" => $xml->weather->tempMaxC->asXML(),
+						"t_min" => $xml->weather->tempMinC->asXML());
+					unset($xml);
+				}
+			}
+		}
+	}
 
+	public function printError()
+	{
+		// Если есть ошибки, то выводим предупреждение
+		if ($this->isErrorXML or $this->isErrorDate or $this->isErrorIncorrectDate)
+		{
+			echo "Ошибка! Города нет в базе данных или неверно введена дата.";
+		}
+	}
+
+	public function saveInServer()
+	{
+		// Если в данных полученных с сервера нет ошибки и Memcache работает - сохраним их на случайном и последнем подключенном сервере
+		if (!$this->isErrorXML and !$this->isErrorDate and !$this->isErrorIncorrectDate and $this->isWorkServer())
+		{
+			//Рандом для сервера, к которому будем подключаться
+			$maxServ = count($this->servers)-1;
+			$randServ = rand (0, $maxServ);
+			//Если рандом не выпал на последний сервер (к которому мы уже подключены), то отключаемся от него и подключаемся к рандомному
+			if ($maxServ != $randServ)
+			{
+				$this->memcache->close($this->servers[$maxServ]);
+				$this->memcache -> connect($this->servers[$randServ], 11211);
+			}
+			//Записываем данные в кэш и храним эти данные сутки
+			$this->memcache->set($this->getKeyMemcache(), $this->info, 86400);
+		}
+	}
+
+	public function printInfo()
+	{
+		//Если нет ошибок - выводим данные
+		if (!$this->isErrorXML and !$this->isErrorIncorrectDate and !$this->isErrorDate)
+		{
+			echo "<br>Вы выбрали город: ".$this->info["town"];
+			echo "<br>Максимальная температура в этот день: ".$this->info["t_max"];
+			echo "<br>Минимальная температура в этот день: ".$this->info["t_min"];
+		}
+	}
 }
 class Weather_day extends BaseWeather
 {
-
 	public  function __construct()
 	{
 		parent::__construct();
@@ -123,44 +236,53 @@ class Weather_day extends BaseWeather
 					</form>';
 	}
 
-	public function isIncorrectDate()
-	{
-		//Создаем объект с исключением (если объект не создался) - фиксируем ошибку
-		try
-		{
-			$this->dateUser = new DateTime($this->getDate());
-			$this->isErrorIncorrectDate=false;
-		}
-		catch (Exception $e)
-		{
-			$this->isErrorIncorrectDate=true;
-		}
-		return $this->isIncorrectDate();
-	}
-
 	//Создаем объект с текущей датой
-	public function createObjectDate()
+	public function isErrorInDate()
 	{
-		//Объект с текущей датой
-		$this->dateToday = new DateTime();
-		date_time_set($this->dateToday, 00, 00, 00);
 		//Добавляем к текущей дате 7 дней
-		$this->date7 = new DateTime('+7 days');
+		$date7 = new DateTime('+7 days');
+		date_time_set($date7, 00, 00, 00);
+		$dateToday = new DateTime();
+		date_time_set ($dateToday, 00, 00, 00);
+		$nowDate = new DateTime();
+		//Если полученная дата не входит в недельный интервал
+		if ($this->getDate() <= $date7 and $this->getDate() >= $dateToday)
+			{
+				$this->isErrorDate = false;
+			}
+			else
+			{
+				$this->isErrorDate = true;;
+			}
+		//Если пользователь ничего не ввел в поле даты
+		if ($this->getDate() == $nowDate)
+		{
+			$this->isErrorDate = true;
+		}
 	}
 
 	public function setRequestMode()
 	{
-		echo "Нажатие кнопки. Перед установкой города и даты<br>";
-		parent::loadServers();
-		parent::connect();
-		parent::separationDate();
+
 	}
 
 	public function doOutput()
 	{
-		echo "Данные после обработки<br>";
-		var_dump($this->isIncorrectDate());
-
+		#var_dump($this->getDate());
+		parent::isWorkServer();
+		parent::loadServers();
+		parent::connect();
+		$this->isErrorInDate();
+		parent::loadApi();
+		parent::decodeJson();
+		parent::decodeXml();
+		parent::printError();
+		parent::saveInServer();
+		#$this->memcache->flush();
+		parent::printInfo();
+		#var_dump($this->isErrorDate);
+		#var_dump($this->isErrorXML);
+		#var_dump($this->isErrorIncorrectDate);
 	}
 }
 
@@ -170,6 +292,5 @@ class Weather_today extends BaseWeather
 
 class Weather_tomorrow extends BaseWeather
 {
-
 }
 ?>
